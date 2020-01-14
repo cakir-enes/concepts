16d9726200e0353de3643c30b440ea3ef17b2b12
cakir-enes <caki-enes@outlook.com>
2020-01-14 22:34:36 +0300

    impl tracking dragged card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,46 @@
 import React, { useState, useRef } from 'react';
 import { Rnd } from "react-rnd";
 
-import { useStore, useContent } from "../stores"
+import { useStore, useContent, useDraggingCard } from "../stores"
 
 export const Card = ({ val, index }) => {
-    
+
     const sizePos = useContent(c => c.sizesAndPoses[index])
-    const [pos, setPos] = useState({x: sizePos.x , y: sizePos.y});
-    const [size, setSize] = useState({w: sizePos.w, h: sizePos.h});
+    const updateSize = useContent(c => c.updateCardSize)
+    const updatePos = useContent(c => c.updateCardPosition)
+    const cardDragged = useDraggingCard(s => s.cardDragged)
+
     const maxWidth = useStore(s => s.maxWidth);
     const ref = useRef();
-    
+
     return (<div>
         <Rnd default={{
-            x: pos.x,
-            y: pos.y,
-            width: size.w,
-            height: size.h
+            x: sizePos.x,
+            y: sizePos.y,
+            width: sizePos.w,
+            height: sizePos.h
         }} onDrag={(e, data) => {
-            // console.log( "max width " + maxWidth + " cur " + (e.clientX + size.w))
-            // console.log("offsetleft" + ref.current)
-            console.log("cc " + (e.clientX + size.w) + " Size: " + (size.w / 2));
-            setPos(data);
-            if (e.clientX + size.w - 150 > maxWidth) {
-                document.getElementsByClassName("view")[0].scrollBy(2, 0);
-                ref.current.updatePosition(data);
+
+            ref.current.updatePosition(data);
+
+        }}
+            onResizeStop={(e, dir, ref, delta, pos) => updateSize(index, { w: ref.style.width, h: ref.style.height })}
+            onDragStop={(e, d) => {
+                cardDragged({index: index,  x: d.x, y: d.y, h: sizePos.h, w: sizePos.w })
+                updatePos(index, { x: d.x, y: d.y })
             }
-            else if (e.clientX + size.w < (size.w / 2)) {
-                document.getElementsByClassName("view")[0].scrollBy(-2, 0);
-                ref.current.updatePosition(data);
             }
-        }} onDragStop={(e, d) => {
-            // setPos({x: d.x, y: d.y});
-            // poses[index] = {x: d.x, y: d.y}
-        }} minWidth={100} minHeight={100} ref={ref} enableUserSelectHack={false}>
+
+            minWidth={100}
+            minHeight={100}
+            ref={ref}
+            enableUserSelectHack={false}>
+
             <div className="box" style={{ background: "blue", margin: 0, height: '100%', padding: '12px' }}>
                 <textarea style={{ width: "100%", height: "100%" }} defaultValue={val} />
                 <button onClick={() => ref.current.updatePosition({ x: pos.x + 5, y: pos.y })}>CLICK</button>
             </div>
+
         </Rnd>
     </div>);
 };
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import create from 'zustand';
+import produce from "immer"
 import { Card } from '../components';
 
 
 export const dims = Array.from({length: 8}, () => ({x: 150, y: 0,  w: 250, h: 250}))
+
+// Log every time state is changed
+const log = config => (set, get, api) => config(args => {
+    // console.log("  applying", args)
+    set(args)
+    console.log("  new state", get())
+  }, get, api)
+  
+  // Turn the set method into an immer proxy
+  const immer = config => (set, get, api) => config(fn => set(produce(fn)), get, api)
 
 export const [useStore] = create(set => ({
     draggingCard: null,
@@ -12,17 +23,25 @@ export const [useStore] = create(set => ({
     updateWidth: w => set({maxWidth: w})
 }))
 
-export const [useDraggingCard] = create(set => ({
-    pos: null,
-    size: null,
-    index: null
-}))
+export const [useDraggingCard] = create(log(immer(set => ({
+    info: {},
+    cardDragged: (info) => set(s => ({info})),
+}))))
 
 
-export const [useContent] = create((set, get) => ({
+export const [useContent] = create(log(immer(set => ({
     content: ["ABC", "VIKVIKVIK", "ZXCZCXCXZ"],
     sizesAndPoses: dims,
     addCard: (card) => set({content: [...content, card]}),
     // remCardWithIndex: (i) => set(s => {content: s.content}),
-    cardWithIndex: (i) => get().content[i]
-}))
+    cardWithIndex: (i) => get().content[i],
+    updateCardSize: (index, newSize) => set(s => {
+        const sizeAndPose = s.sizesAndPoses[index]
+        s.sizesAndPoses[index] = {...sizeAndPose, w: newSize.w, h: newSize.h}
+      }),
+      updateCardPosition: (index, newPos) => set(s => {
+        const sizeAndPose = s.sizesAndPoses[index]
+        s.sizesAndPoses[index] = {...sizeAndPose, x: newPos.x, y: newPos.y}
+      })
+      
+}))))
